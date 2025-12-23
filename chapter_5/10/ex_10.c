b8c5365e98d25d292b4cc0f8a0d290f4912663b3
MXh1n <mxh1n8899@gmail.com>
2025-12-23 16:42:14 +0600

    Update ex_10.c

--- chapter_5/10/ex_10.c
+++ chapter_5/10/ex_10.c
@@ -10,15 +10,18 @@ int main(void){
 
     if (grade < 0 || grade > 100){
         printf("Invalid numerical grade.\n");
-       // return 0;
     }
 
+    //Storing the first digit in tens, eg 14, tens = 1
     tens = grade / 10;
-    
+
+    //switch statement cant be used for case 10 so handling grade = 100 case outside
     if (grade == 100){
         printf("Letter grade: A\n");
     }
 
+
+    //Here first digit from 0-9 is handelled
     switch (tens){
         
         case 0: case 1: case 2: case 3: case 4: case 5: printf("Letter grade: F\n"); break;
