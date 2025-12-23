1f25af4f82db4a2dccfc004437e88db55e96a178
MXh1n <mxh1n8899@gmail.com>
2025-12-23 23:25:43 +0600

    Update ex_2.c

--- chapter_6/02/ex_2.c
+++ chapter_6/02/ex_2.c
@@ -5,11 +5,12 @@ int main(void){
 
     printf("Enter two integers: ");
     scanf("%d %d", &m, &n);
-    
+
+    // while (1) is an infinit loop
     while (1){
         
         if (n == 0){
-            gcd = m; break;
+            gcd = m; break; //break required to exit when n = 0
         }
 
         remainder = m % n;
