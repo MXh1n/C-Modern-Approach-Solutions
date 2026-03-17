5610827f87bf1b29d779cca740cf0053bfd3ae53
MXh1n <mxh1n8899@gmail.com>
2026-03-17 12:15:36 +0600

    Add files via upload

---
+++ ex_1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void){
+	//This program is about displaying a star V
+	printf("               *\n");
+	printf("              *\n");
+	printf("             *\n");
+	printf("        *   *\n");
+	printf("         * *\n");
+	printf("          *\n");
+
+
+	return 0;
+}
